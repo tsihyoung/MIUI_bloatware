607d192e19156779d1be44d355a46a00bb016aaf
Young WU <43809414+tsihyoung@users.noreply.github.com>
2019-09-08 20:53:58 +0800

    scripts disable and uninstall bloatware

---
+++ disableMIUI.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# disable
+adb shell pm disable-user com.miui.contentextension
+adb shell pm disable-user com.miui.fm
+adb shell pm disable-user com.miui.qr
+adb shell pm disable-user com.miui.systemAdSolution
+adb shell pm disable-user com.miui.hybrid
+adb shell pm disable-user com.miui.player
+adb shell pm disable-user com.miui.translation.kingsoft
+adb shell pm disable-user com.miui.voicetrigger
+adb shell pm disable-user com.miui.translation.youdao
+adb shell pm disable-user com.miui.newhome
+adb shell pm disable-user com.miui.video
+adb shell pm disable-user com.miui.translationservice
+adb shell pm disable-user com.miui.hybrid.accessory
+adb shell pm disable-user com.miui.translation.xmcloud
+adb shell pm disable-user com.miui.miwallpaper
+adb shell pm disable-user com.miui.analytics
+adb shell pm disable-user com.miui.yellowpage
+adb shell pm disable-user com.miui.voiceassist
+adb shell pm disable-user com.xiaomi.vipaccount
+adb shell pm disable-user com.xiaomi.gamecenter
+adb shell pm disable-user com.xiaomi.gamecenter.sdk.service
+adb shell pm disable-user com.xiaomi.ab
+adb shell pm disable-user com.sohu.inputmethod.sogou.xiaomi
+adb shell pm disable-user com.android.quicksearchbox
+adb shell pm disable-user com.android.thememanager
+adb shell pm disable-user com.duokan.reader
+# uninstall
+adb shell pm uninstall --user 0 com.miui.fm
+adb shell pm uninstall --user 0 com.miui.qr
+adb shell pm uninstall --user 0 com.miui.systemAdSolution
+adb shell pm uninstall --user 0 com.miui.hybrid
+adb shell pm uninstall --user 0 com.miui.player
+adb shell pm uninstall --user 0 com.miui.translation.kingsoft
+adb shell pm uninstall --user 0 com.miui.voicetrigger
+adb shell pm uninstall --user 0 com.miui.translation.youdao
+adb shell pm uninstall --user 0 com.miui.newhome
+adb shell pm uninstall --user 0 com.miui.video
+adb shell pm uninstall --user 0 com.miui.translationservice
+adb shell pm uninstall --user 0 com.miui.hybrid.accessory
+adb shell pm uninstall --user 0 com.miui.translation.xmcloud
+adb shell pm uninstall --user 0 com.miui.miwallpaper
+adb shell pm uninstall --user 0 com.miui.analytics
+adb shell pm uninstall --user 0 com.miui.yellowpage
+adb shell pm uninstall --user 0 com.miui.voiceassist
+adb shell pm uninstall --user 0 com.xiaomi.gamecenter
+adb shell pm uninstall --user 0 com.xiaomi.gamecenter.sdk.service
+adb shell pm uninstall --user 0 com.xiaomi.ab
+adb shell pm uninstall --user 0 com.sohu.inputmethod.sogou.xiaomi
+adb shell pm uninstall --user 0 com.android.quicksearchbox
+adb shell pm uninstall --user 0 com.android.thememanager
+adb shell pm uninstall --user 0 com.duokan.reader
